c4e5d660f256361770686c1d42d441202f9e46f5
Maurice Falls <falls8019oct@gmail.com>
2020-03-15 06:36:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,7 @@ end
 def adding
   hash ={"muskets"=> 2}
   # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest
+  shipping_manifest[muskets]=2
 
 
   # add 4 gun powder to the shipping_manifest hash below
